d53b729dd317ef39849b118899be3e3a3b5df216
tweeden <tweeden36@gmail.com>
2016-07-21 07:59:16 -0500

    Added a play again button it is working properly

--- color.js
+++ color.js
@@ -1,16 +1,24 @@
-var colors = [
-    "rgb(255, 0, 0)",
-    "rgb(255, 255, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 255, 255)",
-    "rgb(0, 0, 255)",
-    "rgb(255, 0, 255)"
-]
-
+var colors = generateRandomColors(6);
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
+
+resetButton.addEventListener("click", function(){
+    // generate all new colors
+    colors = generateRandomColors(6);
+    // pick a new random color from array
+    pickedColor = pickColor();
+    // change colorDisplay to maych picked Color
+    colorDisplay.textContent = pickedColor;
+    // change colors of squares
+    for(var i = 0; i < squares.length; i++){
+        squares[i].style.background = colors[i];
+    }
+    h1.style.background = "#232323";
+});
 
 colorDisplay.textContent = pickedColor;
 
@@ -25,7 +33,9 @@ for(var i = 0; i < squares.length; i++){
     // compare color of pickedColor
     if(clickedColor === pickedColor){
         messageDisplay.textContent = "Correct!";
+        resetButton.textContent = "Play Again?"
         changeColors(clickedColor);
+        h1.style.background = clickedColor;
     } else {
         this.style.background = "#232323";
         messageDisplay.textContent = "Try Again";
@@ -44,4 +54,25 @@ function changeColors(color){
 function pickColor(){
     var random = Math.floor(Math.random() * colors.length);
     return colors[random];
+}
+
+function generateRandomColors(num) {
+    var arr = []
+        // repeat num times
+    for(var i = 0; i < num; i++){
+        // get random color and puxh into arr
+        arr.push(randomColor())
+
+    }
+    return arr;
+}
+
+function randomColor() {
+    // pick a "red" from 0 - 255
+    var r = Math.floor(Math.random() * 256);
+    // pick a "green" from 0 - 255
+    var g = Math.floor(Math.random() * 256);
+    // pick a "blue" from 0 - 255
+    var b = Math.floor(Math.random() * 256);
+    return "rgb(" + r +", " + g +", " + b +")";
 }
