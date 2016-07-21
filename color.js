6e864ce7627b6d779e6d050754ecbda21bbbc581
tweeden <tweeden36@gmail.com>
2016-07-20 23:42:33 -0500

    Added an event listener to js

--- color.js
+++ color.js
@@ -1,1 +1,24 @@
+var colors = [
+    "rgb(255, 0, 0)",
+    "rgb(255, 255, 0)",
+    "rgb(0, 255, 0)",
+    "rgb(0,255, 255)",
+    "rgb(0, 0, 255)",
+    "rgb(255, 0, 255)"
+]
 
+var squares = document.querySelectorAll(".square");
+var pickedColor = colors[3];
+var colorDisplay = document.getElementById("colorDisplay");
+
+colorDisplay.textContent = pickedColor;
+
+for(var i = 0; i < squares.length; i++){
+    // initial colors to squares here
+    squares[i].style.background = colors[i];
+
+    // click listeners connected to squares here
+    squares[i].addEventListener("click", function(){
+        alert("clicked a square");
+    });
+}
